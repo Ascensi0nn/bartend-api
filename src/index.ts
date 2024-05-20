765e1439dc53c4c25bcd86a04c6cf4a2c4986835
JackyFTW <jackmbarter@gmail.com>
2024-05-19 21:48:39 -0400

    Fixed indentation

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,8 @@ app.use(cors());
 app.use("/api/v1", router);
 
 async function start(): Promise<void> {
-   await init();
-   app.listen(8080, () => console.log("Listening on port 8080..."));
+    await init();
+    app.listen(8080, () => console.log("Listening on port 8080..."));
 }
 
 start().then(() => console.log("Finished initialization"));
